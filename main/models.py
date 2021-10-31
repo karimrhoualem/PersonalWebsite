8d15064df43a13806887c3b2d1b817d647c0d685
Karim Rhoualem <karim.rhoualem@gmail.com>
2021-10-31 13:16:49 -0400

    Added certificates to home page.

--- main/models.py
+++ main/models.py
@@ -124,6 +124,8 @@ class Certificate(models.Model):
     title = models.CharField(max_length=200, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
     is_active = models.BooleanField(default=True)
+    image = models.ImageField(blank=True, null=True, upload_to="certificate")
+    url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.name
