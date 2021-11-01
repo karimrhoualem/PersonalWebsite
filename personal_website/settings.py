55412f4242a79d85797a4a6987f39acf480382d7
Karim Rhoualem <karim.rhoualem@gmail.com>
2021-10-31 22:41:26 -0400

    Temp revert to secret_key

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['PERSONAL_WEBSITE_SECRET_KEY']
+SECRET_KEY = 'django-insecure-yqe&wsntp1p)ghd^o)!xn-r*4$@ftzoqbhtns#r1)!qaaoj9tk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
